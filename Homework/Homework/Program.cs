720b5317b8a773b768c83453f2b878ec567c208e
1111 <Alberkhachatryan01@gmail.com>
2019-06-17 07:54:35 -0700

    Homework1

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
